2b109e23720b5fb25a89dd4134adc7a6914cd4b5
Brady Benner <91232779+BradyBenner@users.noreply.github.com>
2022-02-12 16:06:15 -0700

    Delete Final_Project_1.0 directory

--- Final_Project_1.0/src/main/java/pilot/dao/UpdatePilotDao_Interface.java
+++
@@ -1,8 +0,0 @@
-package pilot.dao;
-
-
-import pilot.entity.Pilot;
-
-public interface UpdatePilotDao_Interface {
-	Pilot savePilot(int pilot_Id, String first_name, String last_name, String license);
-}
--- Final_Project_1.0/src/test/java/com/promineotect/jeep/controller/support/FetchJeepTestSupport.java
+++
@@ -1,6 +0,0 @@
-package com.promineotect.jeep.controller.support;
-/*
-public class FetchJeepTestSupport extends BaseTest{
-
-}
-*/
